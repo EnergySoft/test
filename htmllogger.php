6638e73625087aff2f7543bca5dcb1eb83b72551
EnergySoft <service@energy-soft.net>
2020-02-08 16:28:28 +0200

    canges

--- htmllogger.php
+++ htmllogger.php
@@ -45,17 +45,44 @@
             var_dump($data);
             $result = ob_get_clean();
 
-            $html = "<div class='block dump_object'>";
+            $html = "<div class='block dump'>";
             if($pre) $html.="<pre>";
             $html .= $result;
             if($pre) $html.="</pre>";
             $hmtml.= "</div>";
             $this->print($html);
+
         }
 
 
         public function dump_object($data, $extended = false){
 
+            $html.="<div class='block object'>";
+            $html.="<div class='object_header'>#".get_class($data)."
+                <i class='fa fa-plus-square-o' aria-hidden='true'></i>
+                <i class='fa fa-minus-square-o' aria-hidden='true'></i>
+            </div>";
+            $html.="<div class='block object_content'>";
+
+            if(is_object($data)){
+
+                $html.="<table class='table_object'>";
+                foreach($data as $key => $value){
+                    $html.="<tr>";
+                    $html.="<td class='noborder'>".$key.":</td>";
+                    $html.="<td  class='noborder'>";
+                    $html.=$this->write_var($value);
+                    $html.="</td>";
+                    $html.="</tr>";
+                }
+                $html.="</table>";
+
+            }
+
+            $html.="</div>";
+            $html.="</div>";
+
+            $this->print($html);
 
         }
         
@@ -100,18 +127,7 @@
                     $html.="<td>";
                     if(isset($data[$i][$h])){
                         $d = $data[$i][$h];
-                        if(!is_object($d) && !is_array($d)){
-                            $html.=$d;
-                        } else {
-                            if(is_object($d)){
-                                
-                            }
-                            if(is_array($d)){
-                                $this->buffer_output = true;
-                                $this->dump_simple_array($d);
-                                $html.=$this->buffer;
-                            }
-                        }
+                        $html.= $this->write_var($d);
                     }
                     $html.="</td>";
                 }
@@ -136,24 +152,7 @@
 
                 $row2.="<td>";
 
-                if($detailed && (is_array($value) || is_object($value))){
-
-                    
-
-                    ob_start();
-                    var_dump($value);
-                    $result = ob_get_clean();
-        
-                    if($pre) $row2.="<pre>";
-                    $row2 .= $result;
-                    if($pre) $row2.="</pre>";
-
-
-                } else {
-
-                    $row2.=$value;
-                    
-                }
+                $row2.=$this->write_var($value);
 
                 $row2.="</td>";
             }
@@ -170,7 +169,7 @@
 
             $html .= "</table>";
 
-            $html = "<div class='block scrollblock'>".$html."</div>";
+            $html = "<div class='scrollblock'>".$html."</div>";
 
             $this->print($html);
 
@@ -197,6 +196,7 @@
                 if(!isset($this->old_timestamps[$name])){
                     $this->old_timestamps[$name] = $timestamp;
                 }
+
                 $time_diff = $timestamp - $this->old_timestamps[$name];
                 $this->old_timestamps[$name] = $timestamp;
 
@@ -204,10 +204,46 @@
 
             $html = "
             <div class='block timestamp'>
+                <i class='fa fa-clock-o'></i>
                 <span class='timestamp_time'>[".$time_text."] [".$this->write_ms($time_diff)."]</span>
                 <span class='timestamp_comment'><b>".$comment."</b></span>
-            </div>";
+            </div>
+            ";
 
+            $this->print($html);
+
+        }
+
+        /////////////////////////////////////////////// ERRORS
+
+        public function print_error($text = 'error', $code = ''){
+
+            if($code!=''){
+                $code = '<b>#'.$code.':</b> ';
+            }
+
+            $this->print_block('error','fa fa-exclamation-circle' ,$code.$text);
+
+        }
+
+        public function print_warning($text = 'warning'){
+
+            $this->print_block('warning','fa fa-exclamation-circle', $text);
+
+        }
+
+        public function print_info($text = 'info'){
+
+            $this->print_block('info','fa fa-info-circle',$text);
+            
+        }
+
+        private function print_block($type,$icon,$text){
+
+            $html = "<div class='block infoblock block-".$type."'>";
+            $html.= "<div><i class='".$icon."'></i></div>";
+            $html.= $text;
+            $html.= "</div>";
             $this->print($html);
 
         }
@@ -221,12 +257,12 @@
                 $this->log_timestamp('Group '.$name.' start','group_'.$name);
             }
             $html = "<div class='block group'>
-                            <div class='group_header'>
-                                <i class='fa fa-plus-square-o' aria-hidden='true'></i>
-                                <i class='fa fa-minus-square-o' aria-hidden='true'></i>
-                                <span class='group_name'>GROUP: <b>".$name."</b></span>
-                            </div>
-                            <div class='group_content'>";
+                        <div class='group_header'>
+                            <i class='fa fa-plus-square-o' aria-hidden='true'></i>
+                            <i class='fa fa-minus-square-o' aria-hidden='true'></i>
+                            <span class='group_name'>GROUP: <b>".$name."</b></span>
+                        </div>
+                        <div class='group_content'>";
             $this->print($html);
 
         }
@@ -239,7 +275,47 @@
             if($timestamp){
                 $this->log_timestamp('Group '.$name.' end','group_'.$name);
             }
-            
+
+        }
+
+        /////////////////////////////////////////////// WRITERS
+
+        private function write_ms($ms){
+            if($ms < 1000) return $ms.' ms';
+            if($ms > 1000) return round($ms/1000,3).' s';
+        }
+
+        private function write_var($var){
+
+            if(is_object($var)){
+                $this->buffer_output = true;
+                $this->dump_object($var);
+                return $this->buffer;
+            }
+
+            if(is_array($var)){
+                $this->buffer_output = true;
+                $this->dump_simple_array($var);
+                return $this->buffer;
+            }
+
+            if(is_null($var)){
+                return "<span class='span_var span_null'>null</span>";
+            }
+
+            if(is_bool($var)){
+                if($var){
+                    return "<span class='span_var span_bool bool_true'>true</span>";
+                } else {
+                    return "<span class='span_var span_bool bool_false'>false</span>";
+                }
+            }
+
+            if(is_string($var)){
+                return "<span class='span_var span_string'>\"".$var."\"</span>"; 
+            }
+
+            return $var;
         }
 
         /////////////////////////////////////////////// PRIVATE
@@ -288,6 +364,9 @@
                             $('.group_header').click(function(){
                                 $(this).parent().toggleClass('extended');
                             });
+                            $('.object_header').click(function(){
+                                $(this).parent().toggleClass('extended');
+                            });
                         });
                     </script>
 
@@ -296,7 +375,7 @@
                         body{
                             padding:10px;
                             margin:0px;
-                            font-family: Arial;
+                            font-family: Monaco, Arial;
                             font-size:14px;
                             color:".$text_color.";
                         }
@@ -304,10 +383,12 @@
                         .block{
                             margin-top:2px;
                             margin-bottom:2px;
+                            border-radius:".$border_radius.";
                         }
 
                         table {
                             border-collapse: collapse;
+                            font-size:13px;
                         }
 
                         thead{
@@ -318,12 +399,20 @@
                             border: 1px solid ".$border_table_color.";
                         }
 
+                        table.table_object th, table.table_object td{
+                            vertical-align: top;
+                        }
+
+                        .noborder{
+                            border: 0px;
+                        }
+
                         .group_content{
                             display:none;
                             padding-left:20px;
                         }
 
-                        .group>.group_header>.group_content{
+                        .group.extended>.group_content{
                             display:block;
                         }
 
@@ -349,11 +438,36 @@
                             border-left:2px solid ".$border_color.";
                         }
 
-                        .dump_object{
+                        .object_header{
+                            cursor:pointer;
+                            padding-left:3px;
+                            user-select:none;
+                        }
+
+                        .object_content{
+                            display: none;
+                        }
+
+                        .object.extended>.object_content{
+                            display:block;
+                        }
+
+                        .object>.object_header>.fa-minus-square-o{
+                            display:none;
+                        }
+
+                        .object.extended>.object_header>.fa-minus-square-o{
+                            display:inline;
+                        }
+
+                        .object.extended>.object_header>.fa-plus-square-o{
+                            display:none;
+                        }
+
+                        .dump{
                             padding:5px;
                             margin:3px;
                             border:1px solid ".$border_color.";
-                            border-radius:".$border_radius.";
                         }
 
                         .timestamp{
@@ -361,7 +475,6 @@
                             border:1px solid #EEE;
                             background:#fffed1;
                             padding:5px;
-                            border-radius:".$border_radius.";
                         }
 
                         .scrollblock{
@@ -371,8 +484,51 @@
                         }
 
                         .label{
-                            border-radius:".$border_radius.";
                             padding:3px;
+                        }
+
+                        .infoblock{
+                            padding:5px;
+                        }
+
+                        .infoblock div{
+                            width:20px;
+                            height:20px;
+                            display:inline-block;
+                        }
+
+                        .infoblock i{
+                            font-size:16px;
+                        }
+
+                        .block-error{
+                            color:#FFF;
+                            background:#b80c00;
+                        }
+
+                        .block-warning{
+                            background:#ffdb4a;
+                        }
+
+                        .block-info{
+                            background:#adf1ff;
+                        }
+
+                        .bool_false{
+                            color:#8c0005;
+                        }
+
+                        .bool_true{
+                            color:#0e7303;
+                        }
+
+                        .span_string{
+                            color:#c7254e;
+                        }
+
+                        .span_null{
+                            font-style:italic;
+                            color:#0341fc;
                         }
 
                     </style>
@@ -391,28 +547,50 @@
             return $html;
         }
 
-        private function write_ms($ms){
-            if($ms < 1000) return $ms.' ms';
-            if($ms > 1000) return round($ms/1000,3).' s';
+
+
+    }
+
+    class Animal {
+
+        public $gender = '';
+        public $name = '';
+        public $owners = Array();
+        public $cubs = 0;
+        public $mentor;
+        public $some;
+
+        public function feed(){
+
         }
 
     }
 
+    class Human {
+        public $name;
+        public $phone;
+    }
 
+    $human = new Human;
+    $human->name = 'Nelly';
+    $human->phone = '911';
 
+    $animal = new Animal;
+    $animal->gender = 'male';
+    $animal->name = 'Sissy';
+    $animal->owners = Array('Bobb','Harley');
+    $animal->mentor = $human;
     $data1 = Array(
         
-
-
     );
 
     $test1 = "1213";
     $test2 = Array(1,2,43,32423,6623,43,32423,6623,1,2,43,32423,6623,43,32423,34, 34,345, 567,234, 233, 23423, 34,236623,43,32423,6623,Array(33,33,33));
     $test3 = Array(
-        Array('type'=>'cat', 'gender'=>'male','name'=>'Ricky'),
-        Array('type'=>'cat', 'gender'=>'male','name'=>'Bobby','owners'=>Array('Sam','Lucy','Vahtang')),
-        Array('type'=>'dog', 'gender'=>'female','name'=>'Lucky'),
-        Array('type'=>'dog', 'gender'=>'male','name'=>'Demon','cubs'=>6),
+        Array('type'=>'cat', 'alive'=>true,     'gender'=>'male',   'name'=>'Ricky'),
+        Array('type'=>'cat', 'alive'=>true,     'gender'=>'male',   'name'=>'Bobby','owners'=>Array('Sam','Lucy','Vahtang')),
+        Array('type'=>'dog', 'alive'=>true,     'gender'=>'female', 'name'=>'Lucky'),
+        Array('type'=>'dog', 'alive'=>false,    'gender'=>'male',   'name'=>'Demon','cubs'=>6),
     );
 
 
@@ -420,6 +598,7 @@
 
     $logger->log_path = '/home/vladimir/websites/test';
     $logger->log_name = 'testlog';
+
     $logger->create_log_file();
 
     $logger->open_group('LOG', true);
@@ -428,6 +607,18 @@
     $logger->label('TEST 3','#FFF','#0e00d6');
     $logger->dump_table($test3, false);
     $logger->close_group(true);
+
+    $logger->open_group('WELLS');
+    $logger->print_error('Something wrong','404');
+    $logger->print_warning('Something wrong');
+
+    $logger->open_group('INNER');
+    $logger->print_info('Its okay, dont worry');
+    $logger->close_group();
+
+    $logger->close_group();
+
+    $logger->dump_object($animal);
 
     file_get_contents();
 
